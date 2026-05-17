20611b45b9de153a7a18cd08cb20bb508b1619e8
Stojan Petkovic <petkovicstojan@gmail.com>
2026-05-17 12:50:54 +0200

    update

---
+++ src/scripts/admin/app.js
@@ -0,0 +1,84 @@
+import {
+  initNavigation
+}
+from './navigation.js'
+
+import {
+  initLeadsView
+}
+from './leads-view.js'
+
+import {
+  initFilters
+}
+from './filters.js'
+
+import {
+  initPagination
+}
+from './pagination.js'
+
+import {
+  initKanban
+}
+from './kanban.js'
+
+import {
+  initLeadDetails
+}
+from './lead-details.js'
+
+import {
+  initModals
+}
+from './modals.js'
+
+import {
+  initLeadActions
+}
+from './lead-actions.js'
+
+import {
+  initRealtime
+}
+from './realtime.js'
+
+import {
+  initAuth
+}
+from './auth.js'
+
+import {
+  initExport
+}
+from './export.js'
+
+// =========================
+// INIT CRM APP
+// =========================
+
+export function initCRMApp() {
+
+  initLeadDetails()
+
+  initNavigation()
+
+  initLeadsView()
+
+  initFilters()
+
+  initKanban()
+
+  initPagination()
+
+  initModals()
+
+  initLeadActions()
+
+  initRealtime()
+
+  initAuth()
+
+  initExport()
+
+}
